b040cffbf8193155fde761d79d250bc49498905f
Yasuhiro Shimada <yshimada052@gmail.com>
2019-11-16 02:21:47 -0500

    Initial commit

---
+++ src/main/java/com/skillstorm/data/TimesheetDAO.java
@@ -0,0 +1,9 @@
+package com.skillstorm.data;
+
+public class TimesheetDAO {
+	
+	public User findByUsername(String username) {}
+	public List<Timesheet> findTimesheetsByUser(int id){}
+	public Timesheet findTimesheetById(int id) {}
+	public Timesheet save(Timesheet t) {}
+}
